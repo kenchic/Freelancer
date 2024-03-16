7ff1c2fb7e4d148f3b60401e03fb99fa0e739304
Ing. German Alvarez <ayzcorp@hotmail.com>
2024-03-16 15:18:41 -0500

    Ticket#1 Estructura

---
+++ Src/GrupoXpert/GrupoXpert.Maui/Area/Login/Login.razor.cs
@@ -0,0 +1,16 @@
+﻿using GrupoXpert.Components.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GrupoXpert.Maui.Area.Login
+{
+    public partial class Login : ComponentBase
+    {
+        protected void LoginAccess(Credential credential)
+        {
+
+        }
+    }
+}
